7e184c3e568c7572f662c26f6cf17ca239eff57d
Benjamin J Heckendorn <benjamin.heckendorn@gmail.com>
2023-03-30 15:29:51 -0500

    Room 1-3 fix

--- code/Sample Games/Catskill/Catskill.ino
+++ code/Sample Games/Catskill/Catskill.ino
@@ -3387,7 +3387,7 @@ void fallCheckRobot(int index) {		//Pass in robot object index we are checking f
 					
 			int g = fallingObjectIndex[x];			//Get falling object index # (to make this next part not super long)
 		
-			if (object[g].animate > 7) {			//Object must be falling at at least half speed to kill a robot
+			if (object[g].animate > 5) {			//Object must be falling at at least half speed to kill a robot
 			
 				bool hit = false;
 				
