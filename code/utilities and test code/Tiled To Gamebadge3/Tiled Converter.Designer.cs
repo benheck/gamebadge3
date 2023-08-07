ab2dff55fdd67a762dc63638b7874bc6e8ff5045
Benjamin J Heckendorn <benjamin.heckendorn@gmail.com>
2023-08-07 12:23:31 -0500

    New case and organzing

---
+++ code/utilities and test code/Tiled To Gamebadge3/Tiled Converter.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace Tiled_To_Gamebadge3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.filenameBox = new System.Windows.Forms.TextBox();
+            this.buttonMap = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_0 = new System.Windows.Forms.TextBox();
+            this.textBox_1 = new System.Windows.Forms.TextBox();
+            this.textBox_2 = new System.Windows.Forms.TextBox();
+            this.textBox_3 = new System.Windows.Forms.TextBox();
+            this.textBox_platform = new System.Windows.Forms.TextBox();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOAD CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filenameBox
+            // 
+            this.filenameBox.Location = new System.Drawing.Point(12, 38);
+            this.filenameBox.Name = "filenameBox";
+            this.filenameBox.Size = new System.Drawing.Size(387, 23);
+            this.filenameBox.TabIndex = 1;
+            // 
+            // buttonMap
+            // 
+            this.buttonMap.Location = new System.Drawing.Point(432, 246);
+            this.buttonMap.Name = "buttonMap";
+            this.buttonMap.Size = new System.Drawing.Size(140, 42);
+            this.buttonMap.TabIndex = 2;
+            this.buttonMap.Text = "EXPORT MAP";
+            this.buttonMap.UseVisualStyleBackColor = true;
+            this.buttonMap.Click += new System.EventHandler(this.buttonMap_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select _pal_0.csv file";
+            // 
+            // textBox_0
+            // 
+            this.textBox_0.Location = new System.Drawing.Point(136, 147);
+            this.textBox_0.Name = "textBox_0";
+            this.textBox_0.Size = new System.Drawing.Size(263, 23);
+            this.textBox_0.TabIndex = 4;
+            // textBox_1
+            // 
+            this.textBox_1.Location = new System.Drawing.Point(136, 176);
+            this.textBox_1.Name = "textBox_1";
+            this.textBox_1.Size = new System.Drawing.Size(263, 23);
+            this.textBox_1.TabIndex = 5;
+            // 
+            // textBox_2
+            // 
+            this.textBox_2.Location = new System.Drawing.Point(136, 207);
+            this.textBox_2.Name = "textBox_2";
+            this.textBox_2.Size = new System.Drawing.Size(263, 23);
+            this.textBox_2.TabIndex = 6;
+            // 
+            // textBox_3
+            // 
+            this.textBox_3.Location = new System.Drawing.Point(136, 236);
+            this.textBox_3.Name = "textBox_3";
+            this.textBox_3.Size = new System.Drawing.Size(263, 23);
+            this.textBox_3.TabIndex = 7;
+            // 
+            // textBox_platform
+            // 
+            this.textBox_platform.Location = new System.Drawing.Point(136, 265);
+            this.textBox_platform.Name = "textBox_platform";
+            this.textBox_platform.Size = new System.Drawing.Size(263, 23);
+            this.textBox_platform.TabIndex = 8;
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(71, 100);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(501, 23);
+            this.pathBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Path:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Map Properties:";
+            // 
+            // textBox_1
+            // 
+            this.textBox_1.Location = new System.Drawing.Point(136, 176);
+            this.textBox_1.Name = "textBox_1";
+            this.textBox_1.Size = new System.Drawing.Size(263, 23);
+            this.textBox_1.TabIndex = 5;
+            // 
+            // textBox_2
+            // 
+            this.textBox_2.Location = new System.Drawing.Point(136, 207);
+            this.textBox_2.Name = "textBox_2";
+            this.textBox_2.Size = new System.Drawing.Size(263, 23);
+            this.textBox_2.TabIndex = 6;
+            // 
+            // textBox_3
+            // 
+            this.textBox_3.Location = new System.Drawing.Point(136, 236);
+            this.textBox_3.Name = "textBox_3";
+            this.textBox_3.Size = new System.Drawing.Size(263, 23);
+            this.textBox_3.TabIndex = 7;
+            // 
+            // textBox_platform
+            // 
+            this.textBox_platform.Location = new System.Drawing.Point(136, 265);
+            this.textBox_platform.Name = "textBox_platform";
+            this.textBox_platform.Size = new System.Drawing.Size(263, 23);
+            this.textBox_platform.TabIndex = 8;
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(71, 100);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(501, 23);
+            this.pathBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "PATH:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Exports condoX-X.map";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusLabel.Location = new System.Drawing.Point(429, 136);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(143, 54);
+            this.statusLabel.TabIndex = 12;
+            this.statusLabel.Text = "READY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 302);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.textBox_platform);
+            this.Controls.Add(this.textBox_3);
+            this.Controls.Add(this.textBox_2);
+            this.Controls.Add(this.textBox_1);
+            this.Controls.Add(this.textBox_0);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonMap);
+            this.Controls.Add(this.filenameBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tiled Gamebadge Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filenameBox;
+        private System.Windows.Forms.Button buttonMap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_0;
+        private System.Windows.Forms.TextBox textBox_1;
+        private System.Windows.Forms.TextBox textBox_2;
+        private System.Windows.Forms.TextBox textBox_3;
+        private System.Windows.Forms.TextBox textBox_platform;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ code/utilities and test code/Tiled To Gamebadge3/Tiled Converter.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tiled_To_Gamebadge3
+{
+
+    public partial class Form1 : Form
+    {
+        ushort[,] exportMap = new ushort[15, 120];
+
+        short[,] currentMap = new short[15, 120];
+
+        string condoName;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Beep.Play();
+
+            String fileName = GetFileName("CSV (*.csv)|*.csv");
+            if (fileName != null)
+            {
+                filenameBox.Text = fileName;
+                pathBox.Text = Path.GetDirectoryName(fileName);
+
+                string path = filenameBox.Text;
+                string[] pathArr = path.Split('\\');
+                string[] fileArr = pathArr.Last().Split('.');
+                string[] noExt = fileArr.First().Split('_');
+                condoName = noExt.First().ToString();
+
+                textBox_0.Text = condoName + "_pal0";
+                textBox_1.Text = condoName + "_pal1";
+                textBox_2.Text = condoName + "_pal2";
+                textBox_3.Text = condoName + "_pal3";
+                textBox_platform.Text = condoName + "_platform";
+
+                statusLabel.Text = "LOADED";
+            }
+
+        }
+
+        private String GetFileName(String filter)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = filter;
+            dlg.RestoreDirectory = true;
+            if (filenameBox.Text.Length > 0)
+            {
+                dlg.InitialDirectory = GetCurrentFilePath();
+            }
+            if (dlg.ShowDialog(this) == DialogResult.OK)
+            {
+                return dlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private String GetCurrentFilePath()
+        {
+            return filenameBox.Text.Substring(0, filenameBox.Text.LastIndexOf("\\") + 1);
+        }
+
+        private void buttonMap_Click(object sender, EventArgs e)
+        {
+
+            //Bud Game Palette 3 (B&W, condo background)
+            currentMap = ConvertCSV(pathBox.Text + "\\" + textBox_3.Text + ".csv");        //Convert TILED CSV to a 2D short array
+
+            for (int i = 0; i < 15; i++)                //First pass, fill with blank (32) and add pal 0
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    if (currentMap[i, j] != -1)         //Populated tile on this layer?
+                    {
+                        exportMap[i, j] = (ushort)currentMap[i, j];
+                        exportMap[i, j] |= 0x0300;                          //OR in a match for a Palette 1 tile
+                    }
+                    else
+                    {
+                        exportMap[i, j] = 0x0320;       //If a tile on pal3 was left blank in Tiled (-1), then Default condo is palette 3, space char, no block or plat
+                    }
+                }
+            }
+
+            //Now we draw on top of that layer
+
+            //Palette 0 (gold, probably just the door)
+            currentMap = ConvertCSV(pathBox.Text + "\\" + textBox_0.Text + ".csv");        //Convert TILED CSV to a 2D short array
+
+            for (int i = 0; i < 15; i++)                //First pass, fill with blank (32) and add pal 0
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    //exportMap[i, j] = 0x0320;           //Default condo is palette 3, space char, no block or plat
+
+                    if (currentMap[i, j] != -1)         //Populated tile on this layer?
+                    {
+                        exportMap[i, j] = (ushort)currentMap[i, j];     //Palette is zero, so don't have to OR it in
+                        exportMap[i, j] &= 0x00FF;                      //Lob off any palette color as this is ZERO (such as a null fill with
+                    }
+                }
+            }
+
+            //Palette 1 (brown)
+            currentMap = ConvertCSV(pathBox.Text + "\\" + textBox_1.Text + ".csv");        //Convert TILED CSV to a 2D short array
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    if (currentMap[i, j] != -1)         //Populated tile on this layer?
+                    {
+                        exportMap[i, j] = (ushort)currentMap[i, j];
+                        exportMap[i, j] &= 0x00FF;                      //Lob off any palette color as this is ZERO (such as a null fill with
+                        exportMap[i, j] |= 0x0100;                          //OR in a match for a Palette 1 tile
+                    }
+                }
+            }
+
+            //Palette 2 (blue)
+            currentMap = ConvertCSV(pathBox.Text + "\\" + textBox_2.Text + ".csv");        //Convert TILED CSV to a 2D short array
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    if (currentMap[i, j] != -1)         //Populated tile on this layer?
+                    {
+                        exportMap[i, j] = (ushort)currentMap[i, j];
+                        exportMap[i, j] &= 0x00FF;                      //Lob off any palette color as this is ZERO (such as a null fill with
+                        exportMap[i, j] |= 0x0200;                          //OR in a match for a Palette 1 tile
+                    }
+                }
+            }
+
+            //Platform or blocked layer
+            currentMap = ConvertCSV(pathBox.Text + "\\" + textBox_platform.Text + ".csv");        //Convert TILED CSV to a 2D short array
+
+            for (int i = 0; i < 15; i++)                //First pass, fill with blank (32) and add pal 0
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    if (currentMap[i, j] == 44)         //Set as BLOCKED?
+                    {
+                        exportMap[i, j] |= 0x4000;      //OR in blocked bit
+                    }
+                    if (currentMap[i, j] == 46)         //Set as PLATFORM?
+                    {
+                        exportMap[i, j] |= 0x8000;      //OR in platform bit
+                    }
+
+                }
+            }
+
+            ExportToBinaryFile(exportMap, pathBox.Text + "\\" + condoName + ".map");        //Export it
+
+
+
+            statusLabel.Text = "SAVED";
+        }
+
+        public static short[,] ConvertCSV(string filePath)
+        {
+            var csvLines = File.ReadAllLines(filePath);
+            var numRows = csvLines.Length;
+            var numCols = csvLines[0].Split(',').Length;
+
+            var intArray = new short[numRows, numCols];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                var line = csvLines[i];
+                var values = line.Split(',').Select(short.Parse).ToArray();
+
+                for (int j = 0; j < numCols; j++)
+                {
+                    intArray[i, j] = values[j];
+                }
+            }
+
+            return intArray;
+        }
+
+        public static void ExportToBinaryFile(ushort[,] intArray, string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            using (var writer = new BinaryWriter(stream))
+            {
+                var numRows = intArray.GetLength(0);
+                var numCols = intArray.GetLength(1);
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        byte high = (byte)((intArray[i, j] >> 8) & 0x00FF);
+                        byte low = (byte)(intArray[i, j] & 0x00FF);
+
+                        writer.Write(high);
+                        writer.Write(low);
+                    }
+                }
+
+                byte eot = 128;
+                byte obc = 0;
+                byte eof = 255;
+
+                writer.Write(eot);          //End of tile data
+                writer.Write(obc);            //No objects
+                writer.Write(eof);          //End of file
+
+
+            }
+        }
+
+
+    }
+}
